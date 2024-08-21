81b22d73fd4ec2675d508e18f758e4db82c95666
Mollakhaled <khaledbalok@gmail.com>
2024-08-21 16:40:43 +0600

    delete method added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
     client.connect();
 
     const toysCollection = client.db('bd-toys-market').collection('toys');
-    const bookingCollection = client.db('bd-toys-market').collection('bookings');
+    const bookingCollection = client.db('bd-toys-market').collection('toyBookings');
 
   app.get('/toys', async(req, res)=>{
     const cursor = toysCollection.find();
@@ -40,16 +40,36 @@ async function run() {
     const query = {_id: new ObjectId(id)}
     const options = {
       projection: {
-        sellerName:1, toyName:1, price:1, details:1, picture:1,rating:1
+        sellerName:1, toyName:1,email:1, price:1, details:1, picture:1,rating:1
       }
     }
     const result = await toysCollection.findOne(query, options);
     res.send(result);
   })
   // toys Booking
-  app.post('/bookings', async (req, res) => {
+  app.get('/toyBookings', async(req, res)=>{
+    let query = {};
+    if(req.query?.email){
+      query = {email:req.query.email}
+    }
+    const cursor = bookingCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  })
+
+  app.post('/toyBookings', async (req, res) => {
     const booking = req.body;
     const result = await bookingCollection.insertOne(booking);
+    res.send(result);
+  })
+
+
+
+
+  app.delete('/toyBookings/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await bookingCollection.deleteOne(query);
     res.send(result);
   })
 
